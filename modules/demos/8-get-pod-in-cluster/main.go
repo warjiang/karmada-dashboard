b1fae9a3ff66cebfacd26123915d139853b3ed17
warjiang <1096409085@qq.com>
2024-04-18 21:24:06 +0800

    chore: add demos for proxy client

---
+++ modules/demos/8-get-pod-in-cluster/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubeclient "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+	"net/url"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	kubeConfigPath := os.Getenv("kubeConfigPath")
+	apiConfig, err := GenerateAPIConfigFromKubeconfigFile(kubeConfigPath)
+	if err != nil {
+		panic(err)
+	}
+	originApiServer := apiConfig.Clusters[apiConfig.CurrentContext].Server
+	originApiServerURL, err := url.Parse(originApiServer)
+	if err != nil {
+		panic(err)
+	}
+	clusterName := "member3"
+	memberAPIServer := fmt.Sprintf("%s://%s/apis/cluster.karmada.io/v1alpha1/clusters/%s/proxy", originApiServerURL.Scheme, originApiServerURL.Host, clusterName)
+	apiConfig.Clusters[apiConfig.CurrentContext].Server = memberAPIServer
+	kubeClient, err := ToClientSet(apiConfig)
+	if err != nil {
+		panic(err)
+	}
+	nsList, err := kubeClient.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range nsList.Items {
+		fmt.Println(item.Name)
+	}
+}
+
+func GenerateAPIConfigFromKubeconfigFile(kubeconfigPath string) (*clientcmdapi.Config, error) {
+	// 使用 clientcmd 从 kubeconfig 文件加载配置
+	config, err := clientcmd.LoadFromFile(kubeconfigPath)
+	if err != nil {
+		return nil, err
+	}
+	currentContext := config.CurrentContext
+	context := config.Contexts[currentContext]
+	clusterName := context.Cluster
+	authInfoName := context.AuthInfo
+	cluster := config.Clusters[clusterName]
+	authInfo := config.AuthInfos[authInfoName]
+
+	apiConfig := &clientcmdapi.Config{
+		Clusters: map[string]*clientcmdapi.Cluster{
+			clusterName: cluster,
+		},
+		AuthInfos: map[string]*clientcmdapi.AuthInfo{
+			authInfoName: authInfo,
+		},
+		Contexts: map[string]*clientcmdapi.Context{
+			currentContext: {
+				Cluster:  clusterName,
+				AuthInfo: authInfoName,
+			},
+		},
+		CurrentContext: currentContext,
+	}
+	return apiConfig, nil
+}
+
+// ToClientSet converts a KubeConfig object to a client
+func ToClientSet(config *clientcmdapi.Config) (*kubeclient.Clientset, error) {
+	overrides := clientcmd.ConfigOverrides{Timeout: "10s"}
+	clientConfig, err := clientcmd.NewDefaultClientConfig(*config, &overrides).ClientConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create API client configuration from kubeconfig: %w", err)
+	}
+
+	client, err := kubeclient.NewForConfig(clientConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create API client: %w", err)
+	}
+	return client, nil
+}
